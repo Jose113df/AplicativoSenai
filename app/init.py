88245554ed25b204b2d0ae4e9ae1d78ce251e393
Jose113df <josemateusti@gmail.com>
2024-08-14 11:47:13 -0300

    Aula dia 14/08

--- app/init.py
+++ app/init.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
@@ -6,10 +6,25 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
-
 @app.route('/novoaluno')
 def cadastrar_aluno():
     return render_template('novoaluno.html')
+
+#@app.route('/logar',methods=['POST'])
+#def logar_ra():
+#    ra = request.form['ra']
+#    return f"o RA informado é:"{ra}
+
+@app.route('/diariobordo')
+def abrir_diario():
+    return render_template('diariobordo.html')
+
+@app.route("/logar",methods=['POST'])
+def logar_ra():
+    ra = request.form ['ra']
+    if ra == '12345619':
+        return render_template('diariobordo.html', ra=ra)
+    else:
+        mensagem ="Ra INVALIDA"   
 
 app.run(debug=True)
